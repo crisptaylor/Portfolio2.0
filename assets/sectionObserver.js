f7c2ff3dfd16e5d1a384ee915777ad50dad006cc
Christopher Taylor <customchrisp@gmail.com>
2021-11-25 10:22:56 +0200

    added backtotop button and fixed navbar

--- assets/sectionObserver.js
+++ assets/sectionObserver.js
@@ -1,4 +1,5 @@
 const nav = document.querySelector(".navbar");
+const backtotop = document.querySelector(".backtotop");
 const sectionone = document.querySelector(".section1");
 const brandlogo = document.querySelector(".logo");
 const sectiononeOptions = {
@@ -12,8 +13,10 @@ const sectiononeObserver = new IntersectionObserver(function (entries, sectionon
     entries.forEach(entry => {
         if (!entry.isIntersecting) {
             nav.style.opacity=1;
+            backtotop.style.opacity=1;
         } else {
             nav.style.opacity=0;
+            backtotop.style.opacity = 0;
             
         }
     });
